f500e16fd08502906342da5dfec0b0cabda7dab0
fabian.soto <fabian.soto@unp.gov.co>
2024-11-01 10:13:25 -0500

    linea vida base de datos

---
+++ sesp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class DatosSesp(models.models):
+    serial = models.CharField(primary_key=True, max_length=12)
+    nombre = models.CharField(max_length=200, blank=True, null=True)
+    nombre = models.CharField(max_length=15, blank=True, null=True)
+    departamento = models.CharField(max_length=100, blank=True, null=True)
+    ciudad =  models.CharField(max_length=100, blank=True, null=True)
+    mtsp = models.CharField(max_length=20, blank=True, null=True)
+    nivel_riesgo =  models.CharField(max_length=30, blank=True, null=True)
+    telefono_contacto =  models.CharField(max_length=15, blank=True, null=True)
