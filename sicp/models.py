4e202569974f68bf61255ffde4729742bf849529
Ecosistema-UNP <157920627+andressoto16@users.noreply.github.com>
2024-11-20 16:19:46 -0500

    migración a la base de daTOS

--- sicp/models.py
+++ sicp/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 class DatosProtegido(models.Model):
-    nombre = models.CharField(max_length=255)
-    lider_asignado = models.CharField(max_length=255, null=True, blank=True)
+    primer_nombre = models.CharField(max_length=70)
+    segundo_nombre = models.CharField(max_length=70)
+    primer_apellido = models.CharField(max_length=70)
+    segundo_apellido = models.CharField(max_length=70)
+
+    lider_asignado = models.CharField(max_length=255, null=True, blank=True) # pendiente por revisar
     departamento = models.PositiveSmallIntegerField()
     municipio = models.PositiveIntegerField() 
-    cedula = models.BigIntegerField()
-    enlace_ponal = models.CharField(max_length=255, null=True, blank=True)
+    cedula = models.CharField(max_length=11, blank=True, null=True)
+    enlace_ponal = models.CharField(max_length=255, null=True, blank=True) # pendiente por revisar
     tipo_ruta = models.PositiveSmallIntegerField() #choices=[(1, 'Tipo 1'), (2, 'Tipo 2')]
 
     def __str__(self):
@@ -15,7 +19,7 @@ class DatosProtegido(models.Model):
 
 class Contacto(models.Model):
     id_protegido = models.ForeignKey(DatosProtegido, related_name='contactos', on_delete=models.CASCADE)
-    contacto = models.BigIntegerField()
+    contacto = models.CharField(max_length=11, blank=True, null=True)
 
     def __str__(self):
         return f'Contacto de {self.id_protegido.nombre}'
@@ -23,7 +27,7 @@ class Contacto(models.Model):
 
 class Placa(models.Model):
     id_protegido = models.ForeignKey(DatosProtegido, related_name='placas', on_delete=models.CASCADE)
-    placa = models.CharField(max_length=6)
+    placa = models.CharField(max_length=6, blank=True, null=True)
 
     def __str__(self):
         return self.placa
